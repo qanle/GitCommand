fbf1dad6785ca382ba6d4f0f65992a7718476769
kevin@arrowhitech.com <kevin@arrowhitech.com>
2018-06-20 11:56:27 +0700

    Push solution

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<string> abc = new List<string>
+            {
+                "a",
+                "b",
+                "c"
+            };
+            var t1 = abc.ToList();
+            var t2 = abc as List<string>;
+
+            t1.Add("d");
+            t2.Add("e");
+
+            var a = 0;
+        }
+    }
+}
